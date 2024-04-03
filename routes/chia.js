fc2e82a4b977c4431af11f7ecdbbc28c7872f718
Jodie Kurnia <jodiekurnia@outlook.com>
2024-04-04 02:46:15 +0700

    coba simple

--- routes/chia.js
+++ routes/chia.js
@@ -4,28 +4,28 @@ const router = express.Router();
 
 // Post
 router.get('/register/:serverid/:status', async (req, res) => {
-  const data = new chiaModel({
-      serverid: req.params.serverid,
-      status: req.params.status
-  });
-
+  const { serverid, status } = req.params;
   try {
-      const dataToSave = await data.save();
-      res.status(200).json(dataToSave);
-  }
-  catch (error) {
-      res.status(400).json({ message: error.message });
+    // Find a document with the given serverid
+    let data = await chiaModel.findOneAndUpdate(
+    { serverid }, // Search condition
+    { serverid, status }, // Update or insert data
+    { upsert: true, new: true } // Options: create if not exists, return updated document
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 });
 
 // Get all Method
 router.get('/getAll', async (req, res) => {
   try {
-      const data = await Model.find().select('serverid status -_id');
-      res.json(data);
+    const data = await chiaModel.find({}, { serverid: 1, status: 1, _id: 0 });
+    res.json(data);
   }
   catch (error) {
-      res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 });
 
